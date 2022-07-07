80337a64cd2766a408d4ec79b17779cda9ab09fd
h-buzzi <h-buzzi>
2022-07-07 12:04:54 -0300

    Classe a partir do JSON

---
+++ lib/models/movies.dart
@@ -0,0 +1,30 @@
+class Movies {
+  late int _total;
+  late List<MoviesByYear> _moviesByYear;
+
+  Movies(Map<String, dynamic> json) {
+    _total = json['total'];
+    _moviesByYear = [];
+    if (json['moviesByYear'] != null) {
+      json['moviesByYear'].forEach((v) {
+        _moviesByYear.add(MoviesByYear(v));
+      });
+    }
+  }
+
+  int get total => _total;
+  List<MoviesByYear> get moviesByYear => _moviesByYear;
+}
+
+class MoviesByYear {
+  late int _year;
+  late int _movies;
+
+  MoviesByYear(Map<String, dynamic> json) {
+    _year = json['year'];
+    _movies = json['movies'];
+  }
+
+  int get year => _year;
+  int get movies => _movies;
+}
