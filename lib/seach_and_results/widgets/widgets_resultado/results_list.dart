a0db2a1862fe9cc27bd9549bdc85c847a2e243ea
h-buzzi <h-buzzi>
2022-07-08 20:35:56 -0300

    Refatoramento para evitar repeticao

--- lib/seach_and_results/widgets/widgets_resultado/results_list.dart
+++ lib/seach_and_results/widgets/widgets_resultado/results_list.dart
@@ -6,25 +6,57 @@ import 'package:provider/provider.dart';
 class ResultsList extends StatelessWidget {
   const ResultsList({Key? key}) : super(key: key);
 
+  Text createListText(String text, BuildContext context) {
+    return Text(
+      text,
+      style: Theme.of(context).textTheme.bodyText1,
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
-    List<MoviesByYear> _moviesByYear =
-        Provider.of<MovieAPI>(context).movies.moviesByYear;
+    MovieAPI _movieApi = Provider.of<MovieAPI>(context);
+    List<MoviesByYear> _moviesByYear = _movieApi.movies.moviesByYear;
     return Container(
+      padding: const EdgeInsets.all(7),
       decoration: BoxDecoration(
           color: Theme.of(context).backgroundColor,
           border: Border.all(color: Theme.of(context).shadowColor, width: 1)),
-      child: ListView.builder(
-          itemCount: _moviesByYear.length,
-          itemBuilder: ((context, index) {
-            return Row(
-              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-              children: [
-                Text(_moviesByYear[index].year.toString()),
-                Text(_moviesByYear[index].moviesCount.toString()),
-              ],
-            );
-          })),
+      child: Column(
+        children: [
+          Expanded(
+            child: SizedBox(
+              child: ListView.builder(
+                itemCount: _moviesByYear.length,
+                itemBuilder: ((context, index) {
+                  return Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                    children: [
+                      createListText(
+                          _moviesByYear[index].year.toString(), context),
+                      createListText(
+                          _moviesByYear[index].moviesCount.toString(), context),
+                    ],
+                  );
+                }),
+              ),
+            ),
+          ),
+          Container(
+            color: Theme.of(context).primaryColorLight,
+            width: double.infinity,
+            height: 2,
+          ),
+          Align(
+            alignment: Alignment.centerRight,
+            child: Text(
+              'Total encontrado: ${_movieApi.movies.total.toString()}',
+              style:
+                  Theme.of(context).textTheme.bodyText1!.copyWith(fontSize: 16),
+            ),
+          ),
+        ],
+      ),
     );
   }
 }
